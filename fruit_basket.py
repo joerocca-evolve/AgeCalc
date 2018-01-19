15475fd984d1f978a359a25c0e0a9c0f10b0c36f
root <root@localhost.localdomain>
2018-01-18 21:28:20 -0500

    Cleaned up script

--- fruit_basket.py
+++ fruit_basket.py
@@ -1,8 +1,12 @@
-fruit_basket = ["apple", "orange", "pear", "grape", "strawberry"]
+def guess_fruit(basket):
+    fruit_guess = input("Guess a fruit! Enter your guess below\n")
+    while fruit_guess not in basket:
+        fruit_guess = input("That fruit isn't in the basket. Try again!\n")
+    print("\nYou guessed correctly!")
 
-fruit_guess = input("Guess a fruit! Enter your guess below\n")
+def main():
+    fruit_basket = ["apple", "orange", "pear", "grape", "strawberry"]
+    guess_fruit(fruit_basket)
 
-if fruit_guess in fruit_basket:
-    print("\nYou guess correctly!")
-else:
-    print("\nThat fruit is not in the basket. Try again!")
+
+main()
